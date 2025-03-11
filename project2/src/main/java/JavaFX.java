2686e9e73382c4a8a5e4e25d55d2da9f81359f5f
wli189 <brianleelwt@gmail.com>
2025-03-11 13:09:22 -0500

    update the weather element and add more button

--- project2/src/main/java/JavaFX.java
+++ project2/src/main/java/JavaFX.java
@@ -1,9 +1,19 @@
 import javafx.application.Application;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import weather.Period;
 import weather.WeatherAPI;
@@ -11,7 +21,12 @@ import weather.WeatherAPI;
 import java.util.ArrayList;
 
 public class JavaFX extends Application {
-	TextField temperature,weather;
+	Label titleLabel, temperature, weather;
+	VBox tempBox, root;
+	Font titleFont, tempFont, weatherFont, degreeFont;
+	Text degreeSymbol;
+	StackPane degreePane;
+	Button more;
 
 	public static void main(String[] args) {
 
@@ -21,21 +36,56 @@ public class JavaFX extends Application {
 	//feel free to remove the starter code from this method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("I'm a professional Weather App!");
+		primaryStage.setTitle("Weather");
 		//int temp = WeatherAPI.getTodaysTemperature(77,70);
 		ArrayList<Period> forecast = WeatherAPI.getForecast("LOT",77,70);
 		if (forecast == null){
 			throw new RuntimeException("Forecast did not load");
 		}
-		temperature = new TextField();
-		weather = new TextField();
-		temperature.setText("Today's weather is: "+String.valueOf(forecast.get(0).temperature));
+
+		titleFont = Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 25);
+		titleLabel = new Label("Today's weather");
+		titleLabel.setFont(titleFont);
+
+		temperature = new Label();
+		weather = new Label();
+
+		tempFont = Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 50);
+		temperature.setText(String.valueOf(forecast.get(0).temperature));
+		temperature.setFont(tempFont);
+
+		// set up the degree symbol
+		degreeFont = Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 15);
+		degreeSymbol = new Text("°F");
+		degreeSymbol.setFont(degreeFont);
+
+		// align the degree value and symbol
+		degreePane = new StackPane();
+		degreePane.setAlignment(Pos.CENTER);
+		degreePane.setMaxSize(90,40);
+		degreePane.setMinHeight(40);
+		StackPane.setAlignment(degreeSymbol, Pos.TOP_RIGHT);
+		StackPane.setAlignment(temperature, Pos.CENTER);
+		degreePane.getChildren().addAll(temperature, degreeSymbol);
+
+		weatherFont = Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 20);
 		weather.setText(forecast.get(0).shortForecast);
-		
-		
-		
-				
-		Scene scene = new Scene(new VBox(temperature,weather), 700,700);
+		weather.setFont(weatherFont);
+
+		// create the main VBox with title, temperature, and weather
+		tempBox = new VBox(10);
+		tempBox.getChildren().addAll(titleLabel, degreePane, weather);
+		tempBox.setAlignment(Pos.CENTER);
+
+		more = new Button("More");
+		more.setAlignment(Pos.CENTER);
+
+		root = new VBox(100);
+		root.getChildren().addAll(tempBox, more);
+		root.setAlignment(Pos.CENTER);
+
+		// set up the scene and show the stage
+		Scene scene = new Scene(root, 375, 667);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
