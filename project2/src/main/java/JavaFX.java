7ea81f6a451342b882dbe91d1965344c6eb5323a
hgomeUIC <hgome@uic.edu>
2025-03-12 21:52:53 -0500

    added scene 2

--- project2/src/main/java/JavaFX.java
+++ project2/src/main/java/JavaFX.java
@@ -1,13 +1,14 @@
-	import javafx.application.Application;
+import javafx.application.Application;
 
-	import javafx.geometry.Insets;
-	import javafx.geometry.Pos;
-	import javafx.scene.Scene;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.StackPane;
@@ -19,27 +20,17 @@ import weather.WeatherAPI;
 import java.util.ArrayList;
 
 public class JavaFX extends Application {
-	Label titleLabel, temperature, weather, threeDayForecast, firstDayName, dayTemperature, nightTemperature, precipitation, dayLabel, nightLabel;
-	VBox tempBox, root, root2ndScene, forecastBox, dayAndPrecipVbox, nightAndTempVBox, dayAndTempVBox;
+	Label titleLabel, temperature, weather, threeDayForecast, firstDayName, dayLabel, nightLabel, precipitation, dayTemperature, nightTemperature;
+	VBox tempBox, root, forecastBox, root2, nightAndTempVBox, dayAndTempVBox, dayAndPrecipVbox;
 	HBox firstDayWeatherHbox;
-	Font titleFont,title2ndSceneFont, tempFont, weatherFont, degreeFont, DayFonts;
+	Font titleFont, tempFont, weatherFont, degreeFont, title2ndSceneFont, DayFonts;
 	Text degreeSymbol;
 	StackPane degreePane;
-	Scene basicScene, forecastScene, moreScene;
+	Scene basicScene, forecastScene;
 	Button more;
-	BorderPane sceneTwoBorderPane;
-	public static void main(String[] args) {
-	public class JavaFX extends Application {
-		Label titleLabel, temperature, weather;
-		VBox tempBox, root, root2;
-		Font titleFont, tempFont, weatherFont, degreeFont;
-		Text degreeSymbol;
-		StackPane degreePane;
-		Scene basicScene, forecastScene;
-		Button more;
-		ImageView weatherIcon;
+	ImageView weatherIcon;
 
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 
 		launch(args);
 	}
@@ -69,9 +60,8 @@ public class JavaFX extends Application {
 		dayAndPrecipVbox.getChildren().addAll(firstDayName, precipitation);
 		firstDayWeatherHbox.getChildren().addAll(dayAndPrecipVbox, dayAndTempVBox, nightAndTempVBox);
 		//firstDayWeatherHbox.setAlignment(Pos.CENTER);
-        return firstDayWeatherHbox;
-    }
-
+		return firstDayWeatherHbox;
+	}
 	//feel free to remove the starter code from this method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -82,20 +72,20 @@ public class JavaFX extends Application {
 			throw new RuntimeException("Forecast did not load");
 		}
 
+		// title
 		titleFont = Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 25);
 		titleLabel = new Label("Today's weather");
 		titleLabel.setFont(titleFont);
 
+		// temperature
 		temperature = new Label();
-		weather = new Label();
-
 		tempFont = Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 50);
 		temperature.setText(String.valueOf(forecast.get(0).temperature));
 		temperature.setFont(tempFont);
 
 		// set up the degree symbol
 		degreeFont = Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 15);
-		degreeSymbol = new Text("°F");
+		degreeSymbol = new Text("°" + String.valueOf(forecast.get(0).temperatureUnit));
 		degreeSymbol.setFont(degreeFont);
 
 		// align the degree value and symbol
@@ -107,20 +97,26 @@ public class JavaFX extends Application {
 		StackPane.setAlignment(temperature, Pos.CENTER);
 		degreePane.getChildren().addAll(temperature, degreeSymbol);
 
+		// weather
+		weather = new Label();
 		weatherFont = Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 20);
 		weather.setText(forecast.get(0).shortForecast);
 		weather.setFont(weatherFont);
 
+		// icon
+		weatherIcon = new ImageView(new Image(forecast.get(0).icon));
+		weatherIcon.setFitWidth(50); // resize the icon
+		weatherIcon.setPreserveRatio(true);
+
 		// create the main VBox with title, temperature, and weather
 		tempBox = new VBox(10);
-		tempBox.getChildren().addAll(titleLabel, degreePane, weather);
+		tempBox.getChildren().addAll(titleLabel, degreePane, weather, weatherIcon);
 		tempBox.setAlignment(Pos.CENTER);
 
-			more = new Button("More");
-			more.setAlignment(Pos.CENTER);
-		more.setOnAction(event -> {primaryStage.setScene(moreScene);});
-		// Second scene with the 3 day forecast
-		title2ndSceneFont = Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 25);
+		more = new Button("More");
+		more.setAlignment(Pos.CENTER);
+		more.setOnAction(e -> {primaryStage.setScene(forecastScene);});
+		title2ndSceneFont = Font.font("Arial",FontWeight.BOLD, FontPosture.REGULAR, 25);
 		DayFonts =  Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 15);
 		threeDayForecast = new Label("3-Day Forecast");
 		threeDayForecast.setFont(title2ndSceneFont);
@@ -130,22 +126,18 @@ public class JavaFX extends Application {
 		forecastBox.getChildren().addAll(threeDayForecast, threeDays(1, forecast), threeDays(3, forecast), threeDays(5, forecast));
 		forecastBox.setAlignment(Pos.CENTER);
 
-
-		// -----------------------------------------------------------
 		root = new VBox(100);
 		root.getChildren().addAll(tempBox, more);
 		root.setAlignment(Pos.CENTER);
-		root2ndScene = new VBox(100);
-		//second scene 3-day forecast
-		root2ndScene.getChildren().addAll(forecastBox, firstDayWeatherHbox);
-		sceneTwoBorderPane = new BorderPane(root2ndScene);
-		sceneTwoBorderPane.setPadding(new Insets(10,10,10,10));
-			// set up the scene and show the stage
-			basicScene = new Scene(root, 375, 667);
-		moreScene = new Scene(sceneTwoBorderPane,375, 667 );
-		//		forecastScene = new Scene(root2, 375, 667);
-			primaryStage.setScene(basicScene);
-			primaryStage.show();
-		}
+		root2 = new VBox(100);
+		root2.getChildren().addAll(forecastBox);
+
+		// set up the scene and show the stage
+		basicScene = new Scene(root, 375, 667);
+
+		forecastScene = new Scene(root2, 375, 667);
+		primaryStage.setScene(basicScene);
+		primaryStage.show();
+	}
 
 }
